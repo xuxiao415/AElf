1fadaa57c1f88b895ebd32bd2b703e524ab22445
Dennis-Furrer <dennis.furrer@ibm.com>
2018-05-29 14:09:12 +0100

    RPC CLI - Greeting added

--- AElf.Kernel/Node/RPC/RpcCLI.cs
+++ AElf.Kernel/Node/RPC/RpcCLI.cs
@@ -6,6 +6,12 @@ namespace AElf.Kernel.Node.RPC
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to AElf\n" +
+                              "This is the command-line interface for the RPC API.\n" +
+                              "Please select from the following options:\n" +
+                              "(1) Get Transaction by TxID\n" +
+                              "(2) Insert Transaction\n" +
+                              "(3) Broadcast Transaction\n");
         }
     }
 }
