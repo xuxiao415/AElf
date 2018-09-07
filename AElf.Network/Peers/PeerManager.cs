00476e26504b50153e99cf77d41c8288fa84d8ce
samfrb <samos2x@outlook.com>
2018-09-07 11:02:49 +0800

    Network: added node conf to peer management.

--- AElf.Network/Peers/PeerManager.cs
+++ AElf.Network/Peers/PeerManager.cs
@@ -6,6 +6,8 @@ using System.Net.Sockets;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using AElf.Common.Attributes;
+using AElf.Common.ByteArrayHelpers;
+using AElf.Configuration;
 using AElf.Configuration.Config.Network;
 using AElf.Network.Connection;
 using AElf.Network.Data;
@@ -46,12 +48,48 @@ namespace AElf.Network.Peers
         private Object _peerListLock = new Object(); 
         
         private BlockingCollection<PeerManagerJob> _jobQueue;
+        
+        private readonly List<byte[]> _bpKeys;
+
+        private byte[] _nodeKey;
+        private string _nodeName;
+
+        private bool _isBp;
 
         public PeerManager(IConnectionListener connectionListener, ILogger logger)
         {
             _jobQueue = new BlockingCollection<PeerManagerJob>();
+            _bpKeys = new List<byte[]>();
+            
             _connectionListener = connectionListener;
             _logger = logger;
+            
+            _nodeName = NodeConfig.Instance.NodeName;
+
+            SetBpConfig();
+        }
+        
+        private void SetBpConfig()
+        {
+            var producers = MinersConfig.Instance.Producers;
+
+            // Set the list of block producers
+            try
+            {
+                foreach (var bp in producers.Values)
+                {
+                    byte[] key = ByteArrayHelpers.FromHexString(bp["address"]);
+                    _bpKeys.Add(key);
+                }
+            }
+            catch (Exception e)
+            {
+                _logger?.Warn(e, "Error while reading mining info.");
+            }
+            
+            // This nodes key
+            _nodeKey = ByteArrayHelpers.FromHexString(NodeConfig.Instance.NodeAccount);
+            _isBp = _bpKeys.Any(k => k.BytesEqual(_nodeKey));
         }
 
         public void Start()
